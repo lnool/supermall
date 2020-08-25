ac7af6aebd94fb9d64ff3cb4c81e5998b5f70c0d
e <didoee@88.com>
2020-08-25 16:04:16 +0800

    tab-control

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in title" 
+        :key="index" class="tab-control-item" 
+        :class="{active: currtIndex == index}"
+        @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'TabControl',
+    props:{
+      title:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        currtIndex:0
+      }
+    },
+    methods:{
+      itemClick(index){
+        this.currtIndex = index
+      }
+    }
+  }
+
+</script>
+<style less scoped>
+.tab-control{
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  line-height: 40px;
+  background-color: #fff;
+}
+.tab-control-item{
+  flex:1;
+}
+.tab-control-item span{
+  padding: 5px;
+}
+.active{
+  color: var(--color-high-text);
+}
+.active span {
+  border-bottom: 3px solid var(--color-tint);
+}
+</style>
---
+++ src/views/home/childComps/Feature.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="feature">
+    <a href="">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'Feature',
+    data () {
+      return {
+
+      };
+    },
+    created() {},
+
+    mounted() {},
+
+    update() {},
+
+    computed: {},
+
+    methods: {},
+
+    components: {}
+  }
+
+</script>
+<style less scoped>
+.feature img{
+  width: 100%;
+}
+</style>

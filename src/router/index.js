34cee66679cfb47fe542e184a1de7546e4dcb460
e <didoee@88.com>
2020-08-25 11:22:20 +0800

    项目目录构建

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
+const routes = [{
+  path: '',
+  redirect: '/home'
+},
+{
+  path: '/home',
+  component: () => import('@/views/home/Home.vue')
+},
+{
+  path: '/category',
+  component: () => import('@/views/category/Category.vue')
+},
+{
+  path: '/cart',
+  component: () => import('@/views/cart/Cart.vue')
+},
+{
+  path: '/profile',
+  component: () => import('@/views/profile/Profile.vue')
+}]
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  history: 'history',
+  routes,
 })
 
 export default router
---
+++ src/views/category/Category.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="">
+    分类
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'',
+    data () {
+      return {
+
+      };
+    },
+    created() {},
+
+    mounted() {},
+
+    update() {},
+
+    computed: {},
+
+    methods: {},
+
+    components: {}
+  }
+
+</script>
+<style less scoped>
+
+</style>
